79da168bc56e66cecf2763a8d313a5fb9a18cad0
Mihir Patel <47681434+ToadHanks@users.noreply.github.com>
2020-02-16 16:56:14 -0500

    Update merigold_phyllotaxis.r

--- merigold_phyllotaxis.r
+++ merigold_phyllotaxis.r
@@ -1,25 +1,23 @@
-# Set plot images to a nice size
-options(repr.plot.width = 4, repr.plot.height = 4)
 #Load ggplot2
 library(ggplot2)
 
 # Set the values of angle and points
-angle <- 13*pi/180
+golden_angle <- 13*pi/180 #actual golden angle forumla is Golden Angle= pi * (3 - sqrt(5))
 points <- 2000
 
 #create the formula
-t <- (1:points)*angle
+t <- (1:points) * golden_angle
 x <- sin(t)
 y <- cos(t)
 df <- data.frame(t, x, y)
 
 # create the  flower
-flower_plot <- ggplot(df, aes(x*t, y*t))
-flower_plot + geom_point(size=80, alpha=0.1, shape = 1, color= "indianred4")+ 
-              theme(legend.position="none",
-                panel.background = element_rect(fill="white"),
-                panel.grid=element_blank(),
-                axis.ticks=element_blank(),
-                axis.title=element_blank(),
-                axis.text=element_blank())+
-              ggsave("Phyllotaxis.png",width = 4, height = 4, units = "in")
+ggplot(df, aes(x*t, y*t))+
+    geom_point(size= 80, alpha= 0.1, shape= 1, color= "indianred4")+ 
+    theme(legend.position= "none",
+      panel.background = element_rect(fill= "white"),
+      panel.grid= element_blank(),
+      axis.ticks= element_blank(),
+      axis.title= element_blank(),
+      axis.text= element_blank())+
+    ggsave("Phyllotaxis.png", width= 4, height= 4, units= "in")
