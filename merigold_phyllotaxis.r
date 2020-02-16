dad98f2fe9483b266a8b2a2500a9a5d5dc79682c
Mihir Patel <47681434+ToadHanks@users.noreply.github.com>
2020-02-16 16:10:46 -0500

    Add files via upload

--- merigold_phyllotaxis.r
+++ merigold_phyllotaxis.r
@@ -1,3 +1,6 @@
+# Set plot images to a nice size
+options(repr.plot.width = 4, repr.plot.height = 4)
+#Load ggplot2
 library(ggplot2)
 
 # Set the values of angle and points
@@ -12,11 +15,11 @@ df <- data.frame(t, x, y)
 
 # create the  flower
 flower_plot <- ggplot(df, aes(x*t, y*t))
-flower_plot + geom_point(size=80, alpha=0.1, shape = 1, color= "maroon")+ 
+flower_plot + geom_point(size=80, alpha=0.1, shape = 1, color= "indianred4")+ 
               theme(legend.position="none",
                 panel.background = element_rect(fill="white"),
                 panel.grid=element_blank(),
                 axis.ticks=element_blank(),
                 axis.title=element_blank(),
                 axis.text=element_blank())+
-              ggsave("Phyllotaxis.png",width = 18, height = 18, units = "cm")
+              ggsave("Phyllotaxis.png",width = 4, height = 4, units = "in")
